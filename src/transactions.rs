c651bdb1f6c47024a14dc9bd261d78a4b1f82a9d
Cixtor <cixtords@gmail.com>
2021-08-08 09:00:00 -0800

    Add ability to deserialize account transactions from CSV files

--- src/transactions.rs
+++ src/transactions.rs
@@ -1,9 +1,11 @@
+use serde::Deserialize;
+
 #[derive(Debug)]
 pub struct Transactions {
     txns: Vec<Transaction>,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Deserialize, Clone)]
 pub struct Transaction {
     #[serde(rename = "type")]
     pub tx_type: TransactionType,
@@ -15,11 +17,16 @@ pub struct Transaction {
     pub in_dispute: bool,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Deserialize, Clone)]
 pub enum TransactionType {
+    #[serde(alias = "deposit")]
     Deposit,
+    #[serde(alias = "withdrawal")]
     Withdraw,
+    #[serde(alias = "dispute")]
     Dispute,
+    #[serde(alias = "resolve")]
     Resolve,
+    #[serde(alias = "chargeback")]
     Chargeback,
 }
