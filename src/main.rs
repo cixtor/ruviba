b361db97f3e8ce88fb7099fbeed9907fab17e71a
Cixtor <cixtords@gmail.com>
2021-08-12 09:00:00 -0800

    Read the transactions CSV file from the first argument

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
+use std::env::args;
+use std::error::Error;
+
 #[derive(Debug)]
 pub enum MyErrors {
+    NotEnoughArguments,
     AccountDoesNotExist,
     TransactionAmountIsNone,
     DisputeTransactionDoesNotExist,
@@ -7,5 +11,12 @@ pub enum MyErrors {
 }
 
 fn main() -> Result<(), MyErrors> {
+    let filename = match args().nth(1) {
+        Some(value) => value,
+        None => return Err(MyErrors::NotEnoughArguments),
+    };
+
+    println!("{}", filename);
+
     Ok(())
 }
